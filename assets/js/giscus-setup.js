2e568f32661726223fa55935acd194a80044df9c
code-bytes-ch <acasadof71@gmail.com>
2025-11-27 11:42:00 +0000

    Deploying to gh-pages from @ code-bytes-ch/code-bytes-ch.github.io@3eea81421181ab2436c2126f135fed4c54b99d25 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "alshedivat/al-folio",
+    "data-repo": "code-bytes-ch/code-bytes-ch.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
